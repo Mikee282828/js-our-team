3d2d92b19bc5346a8aed7a8ac26fa8d91be10516
michele laptop <micheledong2803@gmail.com>
2024-05-07 09:51:10 +0200

    tolta maiuscola per le chiavi degli oggetti

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-let team = [{ Nome: "Wayne Barnett", Ruolo: "Founder & CEO", Immagine: "wayne-barnett-founder-ceo.jpg" },
-{ Nome: "Angela Caroll", Ruolo: "Chief Editor", Immagine: "angela-caroll-chief-editor.jpg" },
-{ Nome: "Walter Gordon", Ruolo: "Office Manager", Immagine: "walter-gordon-office-manager.jpg" },
-{ Nome: "Angela Lopez", Ruolo: "Social Media Manager", Immagine: "angela-lopez-social-media-manager.jpg" },
-{ Nome: "Scott Estrada", Ruolo: "Developer", Immagine: "scott-estrada-developer.jpg" },
-{ Nome: "Barbara Ramos", Ruolo: "Graphic Designer", Immagine: "barbara-ramos-graphic-designer.jpg" }
+let team = [{ nome: "Wayne Barnett", ruolo: "Founder & CEO", immagine: "wayne-barnett-founder-ceo.jpg" },
+{ nome: "Angela Caroll", ruolo: "Chief Editor", immagine: "angela-caroll-chief-editor.jpg" },
+{ nome: "Walter Gordon", ruolo: "Office Manager", immagine: "walter-gordon-office-manager.jpg" },
+{ nome: "Angela Lopez", ruolo: "Social Media Manager", immagine: "angela-lopez-social-media-manager.jpg" },
+{ nome: "Scott Estrada", ruolo: "Developer", immagine: "scott-estrada-developer.jpg" },
+{ nome: "Barbara Ramos", ruolo: "Graphic Designer", immagine: "barbara-ramos-graphic-designer.jpg" }
 ];
 
 StampaInfo(team);
@@ -12,10 +12,10 @@ function StampaInfo(array) {
     for (let i = 0; i < array.length; i++) {
         document.querySelector("#areaLavoro").innerHTML +=
            `<div class="card" style="width: 18rem;">
-                <img src="./img/${array[i].Immagine}" class="card-img-top">
+                <img src="./img/${array[i].immagine}" class="card-img-top">
                     <div class="card-body">
-                        <h5 class="card-title">${array[i].Nome}</h5>
-                        <p class="card-text">${array[i].Ruolo}</p>
+                        <h5 class="card-title">${array[i].nome}</h5>
+                        <p class="card-text">${array[i].ruolo}</p>
                     </div>
             </div>`;
     }
